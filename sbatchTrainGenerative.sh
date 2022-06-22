c914748131f9f26f7584ee172fb6f487e0a292a6
israfelsr <israfel.sr@gmail.com>
2022-06-22 15:05:22 +0200

    added: normalization to conditions

--- sbatchTrainGenerative.sh
+++ sbatchTrainGenerative.sh
@@ -1,7 +1,8 @@
 export PYTHONPATH=$(pwd)
 python train/train_generative.py\
     --data_dir=/Users/israfelsalazar/Documents/deep-genomic.nosync/deep-genomic/data/sim1\
-    --condition_files var_current.csv pop.csv\--condition_files var_current.csv pop.csv\
+    --condition_files var_current.csv\
+    --c_norm\
     --num_classes=1\
     --z_dim=6\
     --model=simple_vae\
@@ -9,7 +10,7 @@ python train/train_generative.py\
     --do_haploidization\
     --batch_size=75\
     --criterion=bce_elbo\
-    --num_epochs=200\
+    --num_epochs=10\
     --do_eval\
     --do_encode\
     --compute_r2\
